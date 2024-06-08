21bdb9256980d6c17a70eb8fcac9254d202eaa02
Juan Parra <parrajuan1025@gmail.com>
2024-06-08 19:07:45 -0400

    event more....

--- public/js/renderposts.js
+++ public/js/renderposts.js
@@ -26,7 +26,7 @@ function renderPosts (username) {
 async function loadPosts() {
     let URL = 'https://the-social-network.onrender.com/api/posts';
     let posting = await fetch(URL);
-    const current =posting.results.json()
+    const current =posting.json()
     // console.log(current)
     return current
 }
