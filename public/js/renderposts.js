a19efe426df4d3fcf077e432931e4a260e272784
Juan Parra <parrajuan1025@gmail.com>
2024-06-10 17:46:31 -0400

    more....

--- public/js/renderposts.js
+++ public/js/renderposts.js
@@ -7,7 +7,9 @@
 
 async function loadPosts() {
     let URL = 'https://the-social-network.onrender.com/api/posts';
-    let posting = await fetch(URL);
+    let posting = await fetch(URL).then(()=>{
+        console.log(posting)
+    });
     const current =posting.json()
     const it = Object.getPrototypeOf(current)
     return current
